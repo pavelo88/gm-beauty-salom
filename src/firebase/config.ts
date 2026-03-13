9d73c6b189b447525803d01c18274f5e19bb9246
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-03-13 22:15:44 +0000

    apiKey: "AIzaSyDYzqh32y1GeNkYwaf942aWOyeYqnKCvsA",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDYzqh32y1GeNkYwaf942aWOyeYqnKCvsA",
+  authDomain: "studio-5907958824-7bd63.firebaseapp.com",
+  projectId: "studio-5907958824-7bd63",
+  storageBucket: "studio-5907958824-7bd63.firebasestorage.app",
+  messagingSenderId: "590214528193",
+  appId: "1:590214528193:web:4d241f55ec8f34e19de840"
 };
