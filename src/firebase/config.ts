01ff5993e933e38d10a5ff6fd746f3a6cc1ccda1
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-03-13 21:35:00 +0000

    corrige estos errores porfa studio-5907958824:~/studio{main}$ npx tsc --

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDYzqh32y1GeNkYwaf942aWOyeYqnKCvsA",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-5907958824-7bd63.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-5907958824-7bd63",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-5907958824-7bd63.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "590214528193",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:590214528193:web:4d241f55ec8f34e19de840"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
