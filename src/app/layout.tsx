d23c56bb563e23956c06077ec5d55926b1c3e035
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-03-16 05:44:43 +0000

    quita todos los fondos, todo deja todo opaco para que se vea transparent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -125,7 +125,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className="font-body antialiased text-foreground overflow-x-hidden">
+      <body className="font-body antialiased bg-background text-foreground overflow-x-hidden">
         <FirebaseClientProvider>
           {children}
         </FirebaseClientProvider>
